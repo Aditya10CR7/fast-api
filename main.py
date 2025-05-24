b974d9973c37fa75ac881ca860fc57ed8ef3d1fc
Aditya Pandiarajan <adityapandiarajan7@gmail.com>
2025-05-24 00:15:41 -0700

    Api with CRUD operations

---
+++ main.py
@@ -0,0 +1,43 @@
+from typing import List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Tea(BaseModel):
+    id: int
+    name: str
+    origin: str
+
+teas: List[Tea] = []
+
+@app.get("/")
+def read_root():
+    return {"Message":"Welcome to Home Screen"}
+
+
+@app.get("/teas")
+def get_teas():
+    return teas
+
+@app.post("/teas")
+def add_tea(tea: Tea):
+    teas.append(tea)
+    return tea
+
+@app.put("/teas/{tea_id}")
+def update_tea(tea_id: int, updated_tea: Tea):
+    for index, tea in enumerate(teas):
+        if tea.id == tea_id:
+            teas[index] = updated_tea
+            return updated_tea
+    return {"error": "Tea Not Found"}
+
+@app.delete("/teas/{tea_id}")
+def delete_tea(tea_id: int):
+    for index, tea in enumerate(teas):
+        if tea.id == tea_id:
+            deleted = teas.pop(index)
+            return deleted
+    return {"error": "Tea not found"}
